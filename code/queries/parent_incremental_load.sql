9288fd2406c1ed570a7e4146140ef33b7bff9019
Vikram Kumar Matte <mattevikramkumar@gmail.com>
2026-01-05 14:05:18 -0600

    Add SQL script for incremental load of fact_orders

---
+++ code/queries/parent_incremental_load.sql
@@ -0,0 +1,14 @@
+
+
+COPY INTO fmcg.gold.fact_orders
+FROM(
+  SELECT 
+    CAST(date AS DATE) AS date,
+    product_code,
+    customer_code,
+    CAST(sold_quantity AS BIGINT) AS sold_quantity
+  FROM '/Volumes/fmcg/gold/parent_incremental_update/fact_orders.csv'
+)
+FILEFORMAT = CSV
+FORMAT_OPTIONS ('header' = 'true')
+
